e3c36f9bc3606bd98633970a18339b266e98b67e
DESKTOP-C38B3IC\admin <leehut95@nate.com>
2023-01-04 19:10:32 +0900

    프로젝트1 커밋

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,10 +1,35 @@
+import ProjectCSS from "./Project.module.css";
+
 function Project() {
 
 
     return(
         <>
-        <div>
-            Hi3
+        <div className={ProjectCSS.firstBox}>
+            
+            <div className={ProjectCSS.title}>
+                PROJECT
+            </div>
+            <div className={ProjectCSS.projectBox}>
+                <div className={ProjectCSS.projectBox1}>
+                    <div className={ProjectCSS.project1}>
+                        
+                        <div className={ProjectCSS.project1Box}>
+                            <img src="static/images/프로젝트1.gif" alt="프로젝트1" />
+                        </div>
+                    </div>
+                    <div className={ProjectCSS.project2}>
+                    <div className={ProjectCSS.project2Box}></div>
+                    </div>
+                    <div className={ProjectCSS.project3}>
+                    <div className={ProjectCSS.project3Box}></div>
+                    </div>
+                </div>
+            </div>
+            
+            
+            
+            
         </div>
         </>
         
