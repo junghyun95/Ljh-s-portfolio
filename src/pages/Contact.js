aef35ecec23fa099f287a039473d43f28cc6257d
JungHyun <leehut123@gmail.com>
2023-01-05 17:08:53 +0900

    Main 커밋

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,38 @@
+import ContactCSS from "./Contact.module.css";
+
 function Contact() {
+  const onClickmailhandler = () => {
+    alert("아직 개발중인 단계입니다.");
+  };
 
-
-    return(
-        <>
-        <div>
-            Hi4
+  return (
+    <>
+      <div className={ContactCSS.firstBox}>
+        <div className={ContactCSS.title}>CONTACT</div>
+        <div className={ContactCSS.logoBox}>
+          <div className={ContactCSS.logoBox1}>
+            <div className={ContactCSS.kakao}>
+              <a href="https://open.kakao.com/o/s2cVW3We" target="_blank">
+                <img src="static/images/카카오톡.png" alt="카카오톡" />
+              </a>
+            </div>
+            <div className={ContactCSS.instagram}>
+              <a href="https://www.instagram.com/j_hy__un/" target="_blank">
+                <img src="static/images/인스타그램1.png" alt="인스타그램" />
+              </a>
+            </div>
+            <div className={ContactCSS.mail}>
+              <img
+                src="static/images/메일2.png"
+                alt="메일"
+                onClick={onClickmailhandler}
+              />
+            </div>
+          </div>
         </div>
-        </>
-        
-    );
+      </div>
+    </>
+  );
 }
 
-export default Contact; 
+export default Contact;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,38 @@
+import MainCSS from "./Main.module.css";
+import $ from "jquery";
+
 function Main() {
   return (
     <>
-      <div>메인입니다.</div>
+      <div className={MainCSS.firstBox}>
+        <div className={MainCSS.title}>INTRODUCE</div>
+      </div>
+      <div className={MainCSS.intro}>
+        <div className={MainCSS.intro1}>
+          <div className={MainCSS.introBox}>
+            <p className={MainCSS.cssTyping}>
+              안녕하세요 웹 프론트엔드 개발자 이정현입니다.
+            </p>
+
+            <p className={MainCSS.cssTyping}>
+              새로운 트렌드를 중요시하며, 도전하는것에 대해 두려움이 없고,
+              배움을 즐거워 합니다.
+            </p>
+            <p className={MainCSS.cssTyping}>
+              사용자를 위한 사용자를 위해 항상 생각하고 소통하며 개발하고
+              싶습니다.
+            </p>
+            <p className={MainCSS.cssTyping}>
+              앞으로 더 많은 경험과, 배움을 통해 완벽한 개발자가 되기위해
+              노력하겠습니다.
+            </p>
+          </div>
+
+          <div className={MainCSS.introImg}>
+            <img src="static/images/개발1.gif" alt="개발자" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
